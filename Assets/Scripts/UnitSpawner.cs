6316e9dcdbf4c7b3f886791f1ad8eb8e00bf32f3
toni-del <60567665+toni-del@users.noreply.github.com>
2022-10-30 18:06:00 +0300

    add colors for units

--- Assets/Scripts/UnitSpawner.cs
+++ Assets/Scripts/UnitSpawner.cs
@@ -7,20 +7,57 @@ public class UnitSpawner : MonoBehaviour
     public GameObject unitToSpawn;
     public GameObject allUnits;
     public GameObject arrowLeft, arrowRight;
+    public GameObject chosenCard;
+    public Material[] materials = new Material[4];
+
+    public void changeModeT()
+    {
+        Debug.Log(chosenCard.name);
+        arrowLeft.GetComponent<UnitSpawner>().chosenCard = chosenCard;
+        arrowLeft.SetActive(true);
+        arrowRight.GetComponent<UnitSpawner>().chosenCard = chosenCard;
+        arrowRight.SetActive(true);
+    }
     public void SpawnUnitLeft()
     {
         GameObject newUnit = Instantiate(unitToSpawn, new Vector3(-5, 1, -9), Quaternion.identity);
         newUnit.transform.SetParent(allUnits.transform);
+        newUnit = modifiedUnit(newUnit);
         newUnit.SetActive(true);
-        arrowLeft.SetActive(false);
-        arrowRight.SetActive(false);
+        changeModeF();
     }
     public void SpawnUnitRight()
     {
         GameObject newUnit = Instantiate(unitToSpawn, new Vector3(5, 1, -9), Quaternion.identity);
         newUnit.transform.SetParent(allUnits.transform);
+        newUnit = modifiedUnit(newUnit);
         newUnit.SetActive(true);
+        changeModeF();
+    }
+    public void changeModeF()
+    {
         arrowLeft.SetActive(false);
         arrowRight.SetActive(false);
+    }
+    public GameObject modifiedUnit(GameObject modifUn)
+    {
+        switch (chosenCard.name)
+        {
+            case "First Card":
+                Debug.LogWarning("fasf");
+                modifUn.GetComponent<MeshRenderer>().material = materials[0];
+                break;
+            case "Second Card":
+                modifUn.GetComponent<MeshRenderer>().material = materials[1];
+                break;
+            case "Third Card":
+                Debug.LogWarning("sdag");
+                modifUn.GetComponent<MeshRenderer>().material = materials[2];
+                break;
+            case "Fourth Card":
+                modifUn.GetComponent<MeshRenderer>().material = materials[3];
+                break;
+        }
+        return modifUn;
     }
 }
