bae09461092a97feb590fadaa11ea338fcafa5da
toni-del <60567665+toni-del@users.noreply.github.com>
2022-11-01 00:59:50 +0300

    add mana

---
+++ Assets/Scripts/Mana.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mana : MonoBehaviour
+{
+    public GameObject mana;
+    private void Start()
+    {
+        var couratine = StartCoroutine(ChangeMana());
+    }
+    IEnumerator ChangeMana()
+    {
+        while (true)
+        {
+            if (Convert.ToInt32(mana.GetComponent<Text>().text) != 10)
+                mana.GetComponent<Text>().text = Convert.ToString(Convert.ToInt32(mana.GetComponent<Text>().text) + 1);
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
